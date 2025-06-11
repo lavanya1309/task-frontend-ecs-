e2de85a4355f042e44f144c32b347581d5564c28
lavanya1309 <159130578+lavanya1309@users.noreply.github.com>
2025-06-11 19:18:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,14 @@ resource "aws_security_group" "ecs_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   ingress {
+    description = "Allow HTTP"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     description = "Allow SSH"
     from_port   = 22
